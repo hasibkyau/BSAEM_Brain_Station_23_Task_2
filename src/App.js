6c16d482bc0a24b26948617326264819191ecf92
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2023-03-04 16:17:30 +0600

    added authoring section

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
 
 import banner from './assets/img/GG 2.png';
+import Authoring from './components/Authoring';
 
 function App() {
   return (
@@ -13,15 +14,23 @@ function App() {
       
 
     <img src="assets/img/Ellipse 87.png" className="bg_effect bg_effect--top" alt='bg-effect'/>
-    {/* <img src="assets/img/Ellipse 86.png" className="bg_effect bg_effect--left" alt='bg-effect'/>
-    <img src="assets/img/Ellipse 87.png" className="bg_effect bg_effect--right" alt='bg-effect'/>
+    {/* <img src="assets/img/Ellipse 86.png" className="bg_effect bg_effect--left" alt='bg-effect'/> */}
+    {/* <img src="assets/img/Ellipse 87.png" className="bg_effect bg_effect--right" alt='bg-effect'/>
     <img src="assets/img/Ellipse 88.png" className="bg_effect bg_effect--bottom" alt='bg-effect'/> */}
 
     <NavBar/>
     <br/>
+    {/* Banner: A functional (arrow) component without props */}
     <Banner/>
 
     <img src={banner} className="img-fluid" alt='bg-effect'/>
+
+    {/* Authoring: a functional component with props */}
+    <Authoring 
+    title_first_name={"Effortlessly Create and"} 
+    title_last_name = {"Manage Content"}
+    subtitle = {"Our intuitive authoring interface allows for easy and efficient content creation and management. Create and edit content, images, and videos, and preview your changes in real time."}
+    />
 
 
 
--- src/components/Authoring.js
+++ src/components/Authoring.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import '../css/components.css';
 
-
 function Authoring(props) {
     return (
-        <div>
+
+        <div style={{ marginTop: "-200px" }}>
             <div className="container">
-                <p className="title title--banner">{props.title_first_name}<br/>{props.title_last_name}</p>
+                <p className="title title--banner">{props.title_first_name}<br />{props.title_last_name}</p>
                 <p className="title title--subtitle">{props.subtitle}</p>
-            </div>     
+            </div>
+
+            <div className='m-3'>
+                <button class="toggle-button active" onclick="document.getElementById('page').src='assets/img/Rectangle 6.png'">Component 1</button>
+                <button class="toggle-button " onclick="document.getElementById('page').src='assets/img/Rectangle 7.png'">Component 2</button>
+                <button class="toggle-button " onclick="document.getElementById('page').src='assets/img/Rectangle 8.png'">Component 3</button>
+                <button class="toggle-button " onclick="document.getElementById('page').src='assets/img/Rectangle 9.png'">Component 4</button>
+            </div>
         </div>
+
+
     )
-  }
-    
-  Person.defaultProps = {
+}
+
+Authoring.defaultProps = {
     title_first_name: "Transform Your",
     title_last_name: "Web Experience",
     subtitle: "Transform your website with AEM components designed for flexibility and scalability. Our pre-built components and drag-and-drop editor make it easy for anyone!",
-  
-  }
-    
-  export default Authoring;
+
+}
+
+export default Authoring;
